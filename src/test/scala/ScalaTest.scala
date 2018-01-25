fd5cb636da53788a8b08462b210208f41cfdab2f
delphi.xk <delphi_xk@163.com>
2018-01-25 18:15:53 +0800

    update libsvm convert

--- src/test/scala/ScalaTest.scala
+++ src/test/scala/ScalaTest.scala
@@ -378,7 +378,17 @@ object ScalaTest {
 
     val bufferedWriter = new BufferedWriter(new FileWriter(new File(filePath)))
 
+  }
 
+  def testRowProcess: Unit ={
+    val header = "user_id_md5,user_id,app_jr_f0001,app_jr_f0033,app_jr_f0038,app_jr_f0039,app_jr_f0040,app_jr_f0041,app_jr_f0042,app_jr_f0043,app_jr_f0044,mem_jryh_f0000502,brs_brs_p0001308,mkt_schd_p0001328,mkt_schd_p0001327,brs_jd_f0001,brs_jd_f0002,brs_jd_f0003,brs_jd_f0004,brs_jd_f0005,brs_jd_f0006,brs_jd_f0007,brs_jd_f0008,brs_jd_f0009,brs_jd_f0010,brs_jd_f0011,brs_jd_f0020,brs_jd_f0021,brs_jd_f0022,brs_jd_f0023,brs_jd_f0024,brs_jd_f0025,brs_jd_f0026,brs_jd_f0027,brs_jd_f0028,brs_jd_f0029,brs_jd_f0030,brs_jd_f0031,fin_dlc_f0044,jdmall_ordr_f0001,jdmall_ordr_f0002,jdmall_ordr_f0101,jdmall_ordr_f0103,jdmall_ordr_f0104,jdmall_ordr_f0105,jdmall_ordr_f0106,jdmall_ordr_f0107,jdmall_ordr_f0108,jdmall_ordr_f0109,jdmall_ordr_f0110,jdmall_ordr_f0111,jdmall_ordr_f0112,jdmall_ordr_f0113,jdmall_ordr_f0114,jdmall_ordr_f0115,jdmall_ordr_f0116,jdmall_ordr_f0117,jdmall_ordr_f0118,jdmall_ordr_f0119,jdmall_ordr_f0120,jdmall_ordr_f0121,jdmall_ordr_f0122,jdmall_ordr_f0123,jdmall_ordr_f0124,jdmall_up_m0001,jdmall_up_m0002,jdmall_up_m0005,jdmall_up_m0006,jdmall_up_m0007,jdmall_up_m0008,jdmall_up_m0009,jdmall_up_m0010,jdmall_up_m0011,jdmall_up_m0012,jdmall_up_m0013,jdmall_up_m0014,jdmall_up_m0015,jdmall_up_m0016,jdmall_up_m0017,jdmall_up_m0018,jdmall_up_m0019,jdmall_up_m0020,jdmall_up_m0021,jdmall_up_m0022,jdmall_up_m0023,jdmall_up_m0024,jdmall_up_m0025,jdmall_up_m0026,jdmall_up_m0027,jdmall_up_m0028,jdmall_user_f0001,jdmall_user_f0002,jdmall_user_f0003,jdmall_user_f0004,jdmall_user_f0005,jdmall_user_f0006,jdmall_user_f0007,jdmall_user_f0008,jdmall_user_f0009,jdmall_user_f0010,jdmall_user_f0011,jdmall_user_f0012,jdmall_user_f0013,jdmall_user_f0014,jdmall_user_p0001,jdmall_user_p0002,jdmall_user_p0003,jdmall_user_p0004,jdmall_user_p0005,jdmall_user_p0006,jdmall_user_p0008,jdmall_user_p0010,jdmall_user_p0011,jdmall_user_p0012,jdmall_user_p0019,jdmall_user_p0020,jdmall_user_p0022,jdmall_user_p0023,jdmall_user_p0030,jdmall_user_p0031,jdmall_user_p0032,jdmall_user_p0033,jdmall_user_p0034,jdmall_user_p0035,jdmall_user_p0036,jdmall_user_p0037,jdmall_user_p0056,jdmall_user_p0057,jdmall_user_p0058,jdmall_user_p0059,jdmall_user_p0060,jdmall_user_p0061,jdmall_user_p0062,jdmall_user_p0063,jdmall_user_p0065,jdmall_user_p0066,jdmall_user_p0067,jdmall_user_p0068,jdmall_user_p0072,jdmall_user_p0073,jdmall_user_p0075,mem_edm_f0002,mem_edm_f0003,mem_edm_f0004,mem_edm_f0005,mem_edm_f0007,mem_edm_f0008,mem_edm_f0010,mem_edm_f0011,mem_edm_f0013,mem_edm_f0014,mem_edm_f0015,mem_edm_f0016,mem_edm_f0017,mem_edm_f0018,mem_edm_f0019,mem_edm_f0020,mem_msg_f0001,mem_vip_f0001,mem_vip_f0002,mem_vip_f0003,mem_vip_f0004,mem_vip_f0005,mem_vip_f0006,mem_vip_f0007,mem_vip_f0008,mem_vip_f0010,mem_vip_f0011,mem_vip_f0012,mem_vip_f0013,mem_vip_f0014,mem_vip_f0015,mem_vip_f0016,mem_vip_f0017,mem_vip_f0024,mem_vip_f0025,mem_vip_f0026,mem_vip_f0028,mem_vip_f0029,mem_vip_f0032,mem_vip_f0033,mem_vip_f0023,pay_syt_f0001,pay_syt_f0002,pay_syt_f0003,pay_syt_f0004,pay_syt_f0005,pay_syt_f0006,pay_syt_f0007,pay_syt_f0008,pay_syt_f0009,pay_syt_f0010,pay_syt_f0011,pay_syt_f0012,pay_syt_f0013,pay_syt_f0014,pay_syt_f0015,pay_syt_f0016,pay_syt_f0017,pay_syt_f0018,pay_syt_f0019,pay_syt_f0020,pay_syt_f0021,pay_syt_f0022,pay_syt_f0023,pay_syt_f0024,pay_syt_f0025,pay_syt_f0026,pay_syt_f0027,pay_syt_f0028,pay_syt_f0029,pay_syt_f0030,pay_syt_f0031,pay_syt_f0032,pay_syt_f0033,pay_syt_f0034,pay_syt_f0035,pay_syt_f0036,pay_syt_f0037,pay_syt_f0038,pay_syt_f0039,pay_syt_f0042,pay_syt_f0043,pay_syt_f0044,pay_syt_f0045,pay_syt_f0046,pay_syt_f0047,pay_syt_f0048,pay_syt_f0049,pay_syt_f0050,pay_syt_f0051,pay_syt_f0052,pay_syt_f0053,pay_syt_f0054,pay_syt_f0055,pay_syt_f0056,pay_syt_f0057,pay_syt_f0058,pay_syt_f0059,pay_syt_f0060,pay_syt_f0061,pay_syt_f0062,pay_syt_f0063,pay_syt_f0064,sec_stock_f0000736"
+    val row = "aba890291abfd8d8583c43d781322a26\tjd_e2d9c49735a85\t0,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,2017-10-18 13:11:58.0,2017-10-18 13:11:58.0,1,1,0,,,1,0,,,,,,,,,1,在线支付,快递运输,APP,,,,快递运输,2,1,,,,3,4,,,,,,,2,,,15,1158,15,1158,,,,,,168,0.0,6.0,0.0,0.0,0.0,0.0,1,2,1,1,1,1,,1,56,121.237.137.48,江苏省,南京市,玄武区,a,2017-10-17 10:26:24,15,1158,3412,1,1,,,868869011156923,,9c:5c:8e:c1:e8:cb,,,,,,,0,0,0,0,0,0,0,,0,,,,,,,,,,,,,,,,,,,,,,,,,0,,,,,,,,,,,,,,,,,,,,,,,,,5,,,1315,,,,,0,,,0,,,,1,,,,0,,,,,,0,,,0,,,0,2017-10-18 13:13:43,2017-10-18 13:13:43,2,1,1,WX,1,1,0,,,1,0,,,1,0,0,0,,,,,,,,,"
+    val arrs = row.split("\\t",3)
+    arrs.foreach(println)
+    val feas = arrs(2).split(",",-1)
+    println(feas.length)
+    println(header.split(",").length)
+    println((arrs(0) +: arrs(1) +: feas).length)
   }
 
   def main(args: Array[String]) {
@@ -398,7 +408,7 @@ object ScalaTest {
 //    println(listToString(li))
 
     //iterMap()
-    testHash
+    testRowProcess
 
 
   }
