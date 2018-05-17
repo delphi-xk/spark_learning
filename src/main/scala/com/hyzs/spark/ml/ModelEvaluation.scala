d33639d834d031b9cdaf3ab59b73e3cab533888b
delphi.xk <delphi_xk@163.com>
2018-05-17 18:19:13 +0800

    update model evaluation

--- src/main/scala/com/hyzs/spark/ml/ModelEvaluation.scala
+++ src/main/scala/com/hyzs/spark/ml/ModelEvaluation.scala
@@ -9,57 +9,79 @@ import scala.math.BigDecimal.RoundingMode
   */
 object ModelEvaluation extends App{
 
-  val globalDecimalScale = 5
-  val testData: Array[(BigDecimal, Int)] = readCsvFile("d:/test0515.csv").drop(1)
-    .map(row => (getDecimalValue(row(0).toDouble), row(1).toInt))
-  val posData = testData.filter(_._2 == 1)
-  val negData = testData.filter(_._2 == 0)
-  println(posData.length)
-  println(negData.length)
-
+  // data : score, label
+  val testData: Array[(Double, Int)] = readCsvFile("d:/test0515.csv").drop(1)
+    .map(row => (row(0).toDouble, row(1).toInt))
   val sortedData = testData.sortBy(_._1)
-/*  for(i <- 0 to 20){
-    println(sortedData(i))
-  }*/
-  val threVal = sortedData.map( _._1)
-    .distinct
-  //println(threVal.length)
+  val threVal = sortedData.map( _._1).distinct
 
-/*  for(threshold <- threVal.take(10)){
-    println(threshold, countCumNum(threshold, sortedData, posData.length, negData.length, 5))
-  }*/
+  val ksPointArray: Array[(Double, Double)] = threVal
+    .map( thred => countCumulativeRatio(thred, testData))
+  println(s"ks value: "+ findKSValue(ksPointArray))
 
-  val pointArray: Array[(BigDecimal, BigDecimal)] = threVal.map( thred => countCumNum(thred, testData))
-  println(findKSValue(pointArray))
+  // from 0 to 1
+  val rocPointArray: Array[(Double, Double)] = threVal
+    .map( thred => countTrueAndFalsePositiveRatio(thred, testData))
+    .reverse
+  val simplePoints = simplifyPointByScale(rocPointArray, 3)
+  //val simplePoints = Array( (0.0, 0.0), (0.05, 0.6))
+  for( point <- simplePoints){
+    println(point)
+  }
+  println(s"area under curve: ${areaUnderCurve(simplePoints)}")
 
-
-  def countCumNum(threshold:BigDecimal, data:Array[(BigDecimal, Int)]): (BigDecimal, BigDecimal) = {
+  def countCumulativeRatio(threshold:Double, data:Array[(Double, Int)]): (Double, Double) = {
     val selectData = data.filter(_._1 <= threshold)
     val posData = selectData.count(_._2 == 1)
     val negData = selectData.count(_._2 == 0)
     val totalPos = data.count(_._2 == 1)
     val totalNeg = data.count(_._2 == 0)
-    val posRatio = getDecimalValue(posData) / getDecimalValue(totalPos)
-    val negRatio = getDecimalValue(negData) / getDecimalValue(totalNeg)
-    (posRatio, negRatio)
-  }
-
-/*  def calTrueAndFalsePositive(theshold:BigDecimal, data:Array[(BigDecimal, Int)]): (BigDecimal, BigDecimal) = {
-    val posData = data.filter(_._1 >= theshold)
-    val negData = data.filter(_._1 < theshold)
-
-
-  }*/
-
-  def getDecimalValue(num:Double): BigDecimal={
-    BigDecimal(num).setScale(globalDecimalScale, RoundingMode.HALF_UP)
-  }
-  def findKSValue(pointArray:Array[(BigDecimal, BigDecimal)]): BigDecimal = {
-    val ksVal = pointArray.map(point => math.abs((point._1 - point._2).toDouble))
-    getDecimalValue(ksVal.max)
+    val posRatio = (getDecimalValue(posData) / getDecimalValue(totalPos)).toDouble
+    val negRatio = (getDecimalValue(negData) / getDecimalValue(totalNeg)).toDouble
+    (negRatio, posRatio)
   }
 
 
+  // TPR = TP / P, FPR = FP / N
+  def countTrueAndFalsePositiveRatio(theshold:Double, data:Array[(Double, Int)]): (Double, Double) = {
+    val totalPos = data.count(_._2 == 1)
+    val totalNeg = data.count(_._2 == 0)
+    val TP = data.count(datum => datum._1 >= theshold && datum._2 == 1)
+    val FP = data.count(datum => datum._1 >= theshold && datum._2 == 0)
+    val TPR = (getDecimalValue(TP.toDouble) / getDecimalValue(totalPos)).toDouble
+    val FPR = (getDecimalValue(FP.toDouble) / getDecimalValue(totalNeg)).toDouble
+    (FPR, TPR)
+  }
+
+  def getDecimalValue(num:Double, scale:Int = 5): BigDecimal={
+    BigDecimal(num).setScale(scale, RoundingMode.HALF_UP)
+  }
+  def findKSValue(pointArray:Array[(Double, Double)]): Double = {
+    val ksVal = pointArray.map(point => math.abs(point._1 - point._2))
+    ksVal.max
+  }
+
+  def simplifyPointByScale(pointArray:Array[(Double, Double)],
+                           scale:Integer = 2): Array[(Double, Double)] = {
+    pointArray.map(datum =>
+      (getDecimalValue(datum._1, scale), getDecimalValue(datum._2, scale)) ).distinct
+      .map(datum => (datum._1.toDouble,datum._2.toDouble) )
+  }
+
+  // (0., 0.) , (x_1, y_1) , ... (1. , 1.)
+  def areaUnderCurve(data:Array[(Double, Double)]): Double = {
+    val part1: Array[(Double, Double)] = (0.0, 0.0) +: data
+    val part2: Array[(Double, Double)] = data :+ (1.0, 1.0)
+    part1.zip(part2)
+      .map(pairs => areaUnderTwoPoint(pairs._1._1, pairs._1._2,pairs._2._1, pairs._2._2))
+      .sum
+  }
+
+  def areaUnderTwoPoint(x1:Double, y1:Double, x2:Double, y2:Double): Double = {
+    if(x1 != x2 && y1 != y2){
+      (y1 + y2) * math.abs(x1 - x2) / 2
+    } else 0
+  }
 
 
 }
