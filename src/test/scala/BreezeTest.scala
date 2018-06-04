e6d6991782b9f439fbc970b4978bba0bfc36153d
delphi.xk <delphi_xk@163.com>
2018-06-04 18:46:23 +0800

    update breeze test

--- src/test/scala/BreezeTest.scala
+++ src/test/scala/BreezeTest.scala
@@ -29,6 +29,46 @@ class BreezeTest extends FunSuite{
 
   }
 
+  test("vector operations"){
+    val v3 = DenseVector.rangeD(1,5)
+    val v4 = DenseVector.rangeD(1.0, 10.0, 2.0)
+    val m1 = new DenseMatrix(1,4, v4.toArray)
+    val m2 = new DenseMatrix(4,1, v4.toArray)
+    println(v3)
+    println(v4)
+    println(argmax(v3))
+    println(v3.dot(v4))
+    println(v3 *:* v4)
+    // a.cols == b.length
+    // println(v3 * v4)
+
+    val testM = v3 * m1
+    println(testM)
+    println(testM * v4)
+    val eigenM = eig(testM)
+    println(eigenM.eigenvectors)
+    println(eigenM.eigenvalues)
+    println(testM * eigenM.eigenvectors(::,1))
+    println(eigenM.eigenvalues(1) * eigenM.eigenvectors(::,1))
+
+  }
+
+  test("test matrix multiply"){
+    val v1 = DenseVector(0.1, 0.2, 0.3)
+    val v2 = DenseVector(4.0,5.0,6.0)
+    //println(v1 *:* v2)
+
+    val m1 = new DenseMatrix(3,1, Array(0.1, 0.2, 0.3))
+    val m2 = new DenseMatrix(1,3, Array(4.0,5.0,6.0))
+    val m3 = new DenseMatrix(3,3, Array(1.0, 2.0, 3.0,
+      4.0, 5.0 , 6.0, 7.0,8.0,9.0))
+    println(m3)
+    println(m1)
+    println(m3( ::, 1))
+    println(m3( ::, 1) * 5.0)
+    println( m3 * m1)
+  }
+
   test("test distribution"){
     val gaussian = Gaussian(0.0, 1.0)
     val g_samples = gaussian.sample(100)
@@ -39,8 +79,31 @@ class BreezeTest extends FunSuite{
     val p_sample  = poi.sample(50)
     println(p_sample)
 
-    val gau_matrix = new DenseMatrix(100, 10, gaussian.sample(1000).toArray)
-    println(gau_matrix)
+  }
+
+  test("test matrix funcs"){
+/*    val gaussian = Gaussian(0.0, 1.0)
+    val gau_matrix = new DenseMatrix(4, 4, gaussian.sample(16).toArray)*/
+    val m = DenseMatrix((0.8, 0.8), (0.4, 0.6) )
+    val eigen = eig(m)
+    println("m: ")
+    println(m)
+    println("eigenvalues: ")
+    println(eigen.eigenvalues)
+    println("eigenvectors:")
+    println(eigen.eigenvectors)
+    println("m * eigVector(m): ")
+    println(m * eigen.eigenvectors)
+    println("m * eigVector(0): ")
+    println(m * eigen.eigenvectors(::, 0))
+    println("eigVector(0) * eigenvalues(0) :")
+    println(eigen.eigenvectors(::, 0) * eigen.eigenvalues(0))
+    println("diag(eigValue) * eigVector(m)")
+    println(eigen.eigenvectors * diag(eigen.eigenvalues) )
+    //println("inverse: " + inv(gau_matrix))
+    println(" m ^4")
+    println(m * m * m * m * eigen.eigenvectors(:: , 0))
+    println(m * m * m * m * eigen.eigenvectors(:: , 1))
   }
 
 
